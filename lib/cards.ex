fda9fb42a0e0d5579d4fc38db64470121510d1ce
joaoed <joaoeduardo.dias.silva@gmail.com>
2021-04-22 11:08:55 -0300

    chore: add case tests and doctests

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,17 +1,15 @@
 defmodule Cards do
   @moduledoc """
-    Provides methods for creating and handling a deck of cards
+  Provides methods for creating and handling a deck of cards
   """
 
   @doc """
-    Returns a list of strings representing a deck of playing cards
-
+  Returns a list of strings representing a deck of playing cards
   ## Examples
-        
       iex> deck = Cards.create_deck
-      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> {hand, _deck} = Cards.deal(deck, 1)
       iex> hand
-      ["Ace of spades"]
+      ["Ace of Spades"]
   """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
@@ -25,11 +23,18 @@ defmodule Cards do
 
   def shuffle(deck), do: Enum.shuffle(deck)
 
+  @doc """
+  Determines wheter a doc contains a given card
+  ## Examples
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+  """
   def contains?(deck, hand), do: Enum.member?(deck, hand)
   
   @doc """
-    Divides a deck into a hand and the remainder of the deck.
-    The `hand_size` argument indicates how many cards should be in the hand.
+  Divides a deck into a hand and the remainder of the deck.
+  The `hand_size` argument indicates how many cards should be in the hand.
   """
   def deal(deck, handle_size), do: Enum.split(deck, handle_size)
   
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,21 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 20
   end
+  
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck
+    refute deck == Cards.shuffle(deck)
+  end
+
+  test "get some cards of a deck" do
+    deck = Cards.create_deck
+    {cards, deck} = Cards.deal(deck, 5)
+    assert length(cards) == 5
+  end
+
+
 end
